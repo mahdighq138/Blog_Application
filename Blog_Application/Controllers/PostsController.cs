75418955f73aa529a7d94e37748b4c6970e5e911
mahdi ghasri <mmgh001600@gmail.com>
2025-01-20 11:08:12 +0330

    Controller created and GetAll method added

--- Blog_Application/Controllers/PostsController.cs
+++ Blog_Application/Controllers/PostsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Blog_Application.DB_Context;
+using Blog_Application.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog_Application.Controllers
@@ -7,5 +10,33 @@ namespace Blog_Application.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
+        #region Dependency Injection
+        private readonly BlogDbContext _blogContext;
+
+        public PostsController(BlogDbContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        #endregion
+
+        #region Get All
+        [HttpGet(Name = "GetAll")]
+        [ProducesResponseType(typeof(List<BlogDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetAll()
+        {
+            var posts = _blogContext.Blogs?.ToList();
+
+            if (posts != null || posts!.Count!=0)
+            {
+                return Ok(posts);
+            }
+            return NotFound();
+
+        }
+        #endregion
+
+
     }
 }
