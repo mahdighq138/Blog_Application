1296a1aaf5e8684467dd18cf25ae9185d6b71722
mahdi <mmgh001600@gmail.com>
2025-01-20 09:36:13 +0330

    Add project files.

---
+++ Blog_Application/DTO/BlogDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Application.DTO
+{
+    public class BlogDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Contents { get; set; }
+        public DateTime LastChangeDate { get; set; }
+    }
+}
